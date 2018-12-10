e8e76d7cdc9bb5e1790cd64a84e301b2b348a06d
Denis Craig <dcraig@thousandeyes.com>
2018-12-10 02:44:36 +0000

    Get region from data attribute

--- ec2/asg/main.tf
+++ ec2/asg/main.tf
@@ -45,10 +45,12 @@ resource "aws_autoscaling_group" "asg" {
 
 resource "aws_vpc_endpoint" "ec2_endpoint" {
   vpc_id            = "${var.vpc_id}"
-  service_name      = "com.amazonaws.${var.region}.ec2"
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.ec2"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [
     "${aws_security_group.security_group_common.id}",
   ]
 }
+
+data "aws_region" "current" {}
