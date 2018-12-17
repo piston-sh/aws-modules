4a11efb79c7ee1ac69d4a966fab60d9b152d0098
Denis Craig <dcraig@thousandeyes.com>
2018-12-17 20:33:28 +0000

    Add missing dnsmasq security group rules

--- ec2/security_group_consul/security_group_dns.tf
+++ ec2/security_group_consul/security_group_dns.tf
@@ -79,6 +79,50 @@ resource "aws_security_group_rule" "allow_dns_udp_inbound_from_self" {
   security_group_id = "${aws_security_group.security_group_dns.id}"
 }
 
+resource "aws_security_group_rule" "allow_dnsmasq_tcp_inbound" {
+  count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
+  type        = "ingress"
+  from_port   = "${var.dnsmasq_port}"
+  to_port     = "${var.dnsmasq_port}"
+  protocol    = "tcp"
+  cidr_blocks = ["${var.allowed_inbound_cidr_blocks}"]
+
+  security_group_id = "${aws_security_group.security_group_dns.id}"
+}
+
+resource "aws_security_group_rule" "allow_dnsmasq_udp_inbound" {
+  count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
+  type        = "ingress"
+  from_port   = "${var.dnsmasq_port}"
+  to_port     = "${var.dnsmasq_port}"
+  protocol    = "udp"
+  cidr_blocks = ["${var.allowed_inbound_cidr_blocks}"]
+
+  security_group_id = "${aws_security_group.security_group_dns.id}"
+}
+
+resource "aws_security_group_rule" "allow_dnsmasq_tcp_inbound_from_security_group_ids" {
+  count                    = "${var.allowed_inbound_security_group_count}"
+  type                     = "ingress"
+  from_port                = "${var.dnsmasq_port}"
+  to_port                  = "${var.dnsmasq_port}"
+  protocol                 = "tcp"
+  source_security_group_id = "${element(var.allowed_inbound_security_group_ids, count.index)}"
+
+  security_group_id = "${aws_security_group.security_group_dns.id}"
+}
+
+resource "aws_security_group_rule" "allow_dnsmasq_udp_inbound_from_security_group_ids" {
+  count                    = "${var.allowed_inbound_security_group_count}"
+  type                     = "ingress"
+  from_port                = "${var.dnsmasq_port}"
+  to_port                  = "${var.dnsmasq_port}"
+  protocol                 = "udp"
+  source_security_group_id = "${element(var.allowed_inbound_security_group_ids, count.index)}"
+
+  security_group_id = "${aws_security_group.security_group_dns.id}"
+}
+
 resource "aws_security_group_rule" "allow_dnsmasq_tcp_inbound_from_self" {
   type      = "ingress"
   from_port = "${var.dnsmasq_port}"
