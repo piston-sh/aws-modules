75c39252a92bc1285a9d12d48d9212979d5def03
Denis Craig <dcraig@thousandeyes.com>
2018-12-20 01:09:54 +0000

    Oooook, enable dns support

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr_block}"
   enable_dns_hostnames = false
-  enable_dns_support   = false
+  enable_dns_support   = true
 
   tags {
     name = "${var.name}"
