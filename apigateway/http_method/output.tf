904efce340d758510691aca142adebf244dfa326
Denis Craig <dcraig@thousandeyes.com>
2018-12-28 21:46:04 +0000

    Specify output types

--- apigateway/http_method/output.tf
+++ apigateway/http_method/output.tf
@@ -1,3 +1,4 @@
 output "http_methods" {
+  type = "list"
   value = "${aws_api_gateway_method.http_resource_method.*.http_method}"
 }
--- lambda/rest_function/output.tf
+++ lambda/rest_function/output.tf
@@ -1,3 +1,4 @@
 output "lambda_arns" {
+  type = "list"
   value = "${aws_lambda_function.rest_function.*.arn}"
 }
