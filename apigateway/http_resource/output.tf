302deaf933c069f4d33dd73093c8904c3ceb53dc
Denis Craig <dcraig@thousandeyes.com>
2018-12-28 18:35:49 +0000

    Add deployment stage name as output for http resource

--- apigateway/http_resource/output.tf
+++ apigateway/http_resource/output.tf
@@ -1,3 +1,7 @@
 output "resource_id" {
   value = "${aws_api_gateway_resource.http_resource.id}"
 }
+
+output "deployment_stage_name" {
+  value = "${aws_api_gateway_deployment.http_resource_deployment.stage_name}"
+}
