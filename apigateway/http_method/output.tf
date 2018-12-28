0985628017ad6cb72e20038d03636492a4f03a2b
Denis Craig <dcraig@thousandeyes.com>
2018-12-28 22:34:45 +0000

    nooooope

--- apigateway/http_method/output.tf
+++ apigateway/http_method/output.tf
@@ -1,4 +1,3 @@
 output "http_methods" {
-  type = "list"
   value = "${aws_api_gateway_method.http_resource_method.*.http_method}"
 }
--- lambda/rest_function/output.tf
+++ lambda/rest_function/output.tf
@@ -1,4 +1,3 @@
 output "lambda_arns" {
-  type = "list"
   value = "${aws_lambda_function.rest_function.*.arn}"
 }
