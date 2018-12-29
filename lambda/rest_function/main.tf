c0f6e927ec75fb64948b2ba8d855043558ce6004
Denis Craig <dcraig@thousandeyes.com>
2018-12-29 00:08:04 +0000

    Some cleanup

--- lambda/rest_function/main.tf
+++ lambda/rest_function/main.tf
@@ -1,40 +1,30 @@
 resource "aws_lambda_function" "rest_function" {
-  count = "${length(keys(var.function_key_map))}"
-
-  s3_bucket = "${var.s3_bucket_id}"
-  s3_key    = "${element(values(var.function_key_map), count.index)}"
-
+  count         = "${length(keys(var.function_key_map))}"
+  s3_bucket     = "${var.s3_bucket_id}"
+  s3_key        = "${element(values(var.function_key_map), count.index)}"
   function_name = "${var.cluster_name}_${element(keys(var.function_key_map), count.index)}"
   role          = "${aws_iam_role.lambda_role.arn}"
   handler       = "${var.handler}"
   timeout       = "${var.function_timeout}"
-
-  runtime = "${var.runtime}"
+  runtime       = "${var.runtime}"
 }
 
 resource "aws_lambda_permission" "rest_function_api_gateway_permission" {
-  count = "${length(keys(var.function_key_map))}"
-
-  statement_id = "${var.cluster_name}_${element(keys(var.function_key_map), count.index)}_allow_execution_from_gateway"
-
+  count         = "${length(keys(var.function_key_map))}"
+  statement_id  = "${var.cluster_name}_${element(keys(var.function_key_map), count.index)}_allow_execution_from_gateway"
   action        = "lambda:InvokeFunction"
   function_name = "${element(aws_lambda_function.rest_function.*.function_name, count.index)}"
-
-  principal = "apigateway.amazonaws.com"
-
-  source_arn = "${var.rest_api_execution_arn}/*/*/*"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.rest_api_execution_arn}/*/*/*"
 }
 
 resource "aws_lambda_permission" "rest_function_release_bucket_permission" {
-  count = "${length(keys(var.function_key_map))}"
-
-  statement_id = "${var.cluster_name}_${element(keys(var.function_key_map), count.index)}_release_bucket_permission"
-
+  count         = "${length(keys(var.function_key_map))}"
+  statement_id  = "${var.cluster_name}_${element(keys(var.function_key_map), count.index)}_release_bucket_permission"
   action        = "lambda:InvokeFunction"
   function_name = "${element(aws_lambda_function.rest_function.*.arn, count.index)}"
-
-  principal  = "s3.amazonaws.com"
-  source_arn = "${var.s3_bucket_arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${var.s3_bucket_arn}"
 }
 
 data "aws_region" "current" {}
