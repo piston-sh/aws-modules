7c9afa75e3f6d4263f3a02758f3c5201ed7e2441
Denis Craig <dcraig@thousandeyes.com>
2018-12-30 20:45:15 +0000

    Formatting

--- lambda/rest_function/main.tf
+++ lambda/rest_function/main.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "rest_function" {
-  count         = "${length(keys(var.function_key_map))}"
+  count         = "${length(keys(var.method_function_map))}"
   s3_bucket     = "${var.s3_bucket_id}"
-  s3_key        = "${element(values(var.function_key_map), count.index)}"
-  function_name = "${var.cluster_name}_${lower(element(keys(var.function_key_map), count.index))}"
+  s3_key        = "${element(values(var.method_function_map), count.index)}"
+  function_name = "${var.cluster_name}_${lower(element(keys(var.method_function_map), count.index))}"
   role          = "${element(aws_iam_role.lambda_role.*.arn, count.index)}"
   handler       = "${var.handler}"
   timeout       = "${var.function_timeout}"
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "rest_function" {
 }
 
 resource "aws_lambda_permission" "rest_function_api_gateway_permission" {
-  count         = "${length(keys(var.function_key_map))}"
+  count         = "${length(keys(var.method_function_map))}"
   statement_id  = "${var.cluster_name}_${lower(element(keys(var.method_function_map), count.index))}_allow_execution_from_gateway"
   action        = "lambda:InvokeFunction"
   function_name = "${element(aws_lambda_function.rest_function.*.function_name, count.index)}"
@@ -19,7 +19,7 @@ resource "aws_lambda_permission" "rest_function_api_gateway_permission" {
 }
 
 resource "aws_lambda_permission" "rest_function_release_bucket_permission" {
-  count         = "${length(keys(var.function_key_map))}"
+  count         = "${length(keys(var.method_function_map))}"
   statement_id  = "${var.cluster_name}_${lower(element(keys(var.method_function_map), count.index))}_release_bucket_permission"
   action        = "lambda:InvokeFunction"
   function_name = "${element(aws_lambda_function.rest_function.*.arn, count.index)}"
