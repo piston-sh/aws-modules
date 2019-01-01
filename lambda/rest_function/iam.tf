dbece721044a294a1eb87f858e5a61d7153d49a6
Denis Craig <dcraig@thousandeyes.com>
2019-01-01 22:01:41 +0000

    Add log policy to rest function iam role

--- lambda/rest_function/iam.tf
+++ lambda/rest_function/iam.tf
@@ -18,3 +18,34 @@ resource "aws_iam_role" "lambda_role" {
 }
 EOF
 }
+
+resource "aws_iam_policy" "lambda" {
+  name_prefix = "rest_function_policy-"
+  policy      = "${data.template_file.lambda_policy.rendered}"
+}
+
+resource "aws_iam_policy_attachment" "lambda_attachment" {
+  name  = "rest_function_policy_attachment-"
+
+  policy_arn = "${aws_iam_policy.lambda.arn}"
+  roles      = ["${aws_iam_role.lambda_role.*.name}"]
+}
+
+data "template_file" "lambda_policy" {
+  template = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
--- lambda/rest_function/output.tf
+++ lambda/rest_function/output.tf
@@ -1,3 +1,7 @@
 output "lambda_arns" {
   value = "${zipmap(keys(var.method_function_map), aws_lambda_function.rest_function.*.arn)}"
 }
+
+output "lambda_iam_role_names" {
+  value = "${zipmap(keys(var.method_function_map), aws_iam_role.lambda_role.*.name)}"
+}
