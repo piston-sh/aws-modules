dd118500ab273c773d846a003610d1dd1fcf635d
Denis Craig <admin@deniscraig.com>
2019-01-27 06:24:22 +0000

    Require Authorization header as method param if auth enabled

--- apigateway/http_method/main.tf
+++ apigateway/http_method/main.tf
@@ -5,6 +5,10 @@ resource "aws_api_gateway_method" "http_resource_method" {
   http_method   = "${var.http_method}"
   authorization = "${length(var.cognito_authorizer_id) > 0 ? "COGNITO_USER_POOLS" : "NONE"}"
   authorizer_id = "${length(var.cognito_authorizer_id) > 0 ? "${var.cognito_authorizer_id}" : ""}"
+
+  request_parameters {
+    "method.request.header.Authorization" = "${length(var.cognito_authorizer_id > 0)}"
+  }
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
