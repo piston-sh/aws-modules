dce53c81a09a9f14e7bd304cfced724413bd850e
Denis Craig <admin@deniscraig.com>
2019-01-27 04:10:04 +0000

    Update EC2 ASG IAM role name prefixes

--- ec2/asg/iam.tf
+++ ec2/asg/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_instance_profile" "instance_profile" {
-  name_prefix = "${var.cluster_name}"
+  name_prefix = "${var.cluster_name}-"
   role        = "${aws_iam_role.instance_role.name}"
 
   lifecycle {
@@ -8,7 +8,7 @@ resource "aws_iam_instance_profile" "instance_profile" {
 }
 
 resource "aws_iam_role" "instance_role" {
-  name_prefix        = "${var.cluster_name}"
+  name_prefix        = "${var.cluster_name}-"
   assume_role_policy = "${data.aws_iam_policy_document.instance_role.json}"
 
   lifecycle {
