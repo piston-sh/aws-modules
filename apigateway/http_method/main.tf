57b0f68c54300a3b90ee40dfcd857dad84e0d92f
Denis Craig <admin@deniscraig.com>
2019-01-27 20:25:44 +0000

    Let's do a regex replace instead

--- apigateway/http_method/main.tf
+++ apigateway/http_method/main.tf
@@ -1,11 +1,18 @@
 resource "aws_api_gateway_method" "http_resource_method" {
-  count              = "${var.enabled ? 1 : 0}"
-  rest_api_id        = "${var.rest_api_id}"
-  resource_id        = "${var.resource_id}"
-  http_method        = "${var.http_method}"
-  authorization      = "${length(var.cognito_authorizer_id) > 0 ? "COGNITO_USER_POOLS" : "NONE"}"
-  authorizer_id      = "${length(var.cognito_authorizer_id) > 0 ? "${var.cognito_authorizer_id}" : ""}"
-  request_parameters = "${merge(zipmap(compact(concat(split(",", length(var.cognito_authorizer_id) > 0 ? "method.request.header.Authorization" : ""), list("method.request.header.Host"))), compact(concat(split(",", length(var.cognito_authorizer_id) > 0 ? "true" : ""), list("true")))), var.custom_request_parameters)}"
+  count         = "${var.enabled ? 1 : 0}"
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "${var.http_method}"
+  authorization = "${length(var.cognito_authorizer_id) > 0 ? "COGNITO_USER_POOLS" : "NONE"}"
+  authorizer_id = "${length(var.cognito_authorizer_id) > 0 ? "${var.cognito_authorizer_id}" : ""}"
+
+  request_parameters = "${merge(
+    zipmap(
+      compact(concat(split(",", length(var.cognito_authorizer_id) > 0 ? "method.request.header.Authorization" : ""), list("method.request.header.Host"))), 
+      compact(concat(split(",", length(var.cognito_authorizer_id) > 0 ? "true" : ""), list("true")))
+    ), 
+    var.custom_request_parameters
+  )}"
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
