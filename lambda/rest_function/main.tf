7e738058603e7e034baf5693ded8da70879b328e
Denis Craig <admin@deniscraig.com>
2019-04-25 20:55:26 +0100

    Fix rest function name

--- lambda/rest_function/main.tf
+++ lambda/rest_function/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "rest_function" {
   count         = var.enabled ? length(keys(var.method_function_map)) : 0
   s3_bucket     = var.s3_bucket_id
   s3_key        = element(values(var.method_function_map), count.index)
-  function_name = "${var.cluster_name}_${var.group_name}_${lower(element(keys(var.method_function_map), count.index))
+  function_name = "${var.cluster_name}_${var.group_name}_${lower(element(keys(var.method_function_map), count.index))}"
   role          = element(aws_iam_role.lambda_role.*.arn, count.index)
   handler       = var.runtime == "go1.x" ? "${var.group_name}_${lower(element(keys(var.method_function_map), count.index))}" : var.handler
   timeout       = var.function_timeout
