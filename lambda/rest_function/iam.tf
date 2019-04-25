5c1bb837ce325b0f3cb1119a00bb1374ec3670d7
Denis Craig <admin@deniscraig.com>
2019-04-25 20:53:38 +0100

    Fix some variable interp

--- lambda/rest_function/iam.tf
+++ lambda/rest_function/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  count       = "${var.enabled ? length(keys(var.method_function_map)) : 0}"
+  count       = var.enabled ? length(keys(var.method_function_map)) : 0
   name_prefix = "${var.group_name}_${element(keys(var.method_function_map), count.index)}-"
 
   assume_role_policy = <<EOF
@@ -20,23 +20,23 @@ EOF
 }
 
 resource "aws_iam_policy" "lambda" {
-  count       = "${var.enabled ? 1 : 0}"
+  count       = var.enabled ? 1 : 0
   name_prefix = "${var.group_name}-"
-  policy      = "${data.aws_iam_policy_document.lambda_policy.json}"
+  policy      = data.aws_iam_policy_document.lambda_policy.json
 }
 
 resource "aws_iam_policy_attachment" "lambda_attachment" {
-  count      = "${var.enabled ? 1 : 0}"
+  count      = var.enabled ? 1 : 0
   name       = "rest_function_policy_attachment"
-  policy_arn = "${aws_iam_policy.lambda.arn}"
-  roles      = ["${aws_iam_role.lambda_role.*.name}"]
+  policy_arn = aws_iam_policy.lambda.arn[0]
+  roles      = aws_iam_role.lambda_role.*.name
 }
 
 resource "aws_iam_policy_attachment" "lambda_custom_attachment" {
-  count      = "${var.enabled && length(var.custom_policy_arn) > 0 ? 1 : 0}"
+  count      = var.enabled && length(var.custom_policy_arn) > 0 ? 1 : 0
   name       = "custom_rest_function_policy_attachment"
-  policy_arn = "${var.custom_policy_arn}"
-  roles      = ["${aws_iam_role.lambda_role.*.name}"]
+  policy_arn = var.custom_policy_arn
+  roles      = aws_iam_role.lambda_role.*.name
 }
 
 data "aws_iam_policy_document" "lambda_policy" {
--- lambda/rest_function/main.tf
+++ lambda/rest_function/main.tf
@@ -1,32 +1,32 @@
 resource "aws_lambda_function" "rest_function" {
-  count         = "${var.enabled ? length(keys(var.method_function_map)) : 0}"
-  s3_bucket     = "${var.s3_bucket_id}"
-  s3_key        = "${element(values(var.method_function_map), count.index)}"
-  function_name = "${var.cluster_name}_${var.group_name}_${lower(element(keys(var.method_function_map), count.index))}"
-  role          = "${element(aws_iam_role.lambda_role.*.arn, count.index)}"
-  handler       = "${var.runtime == "go1.x" ? "${var.group_name}_${lower(element(keys(var.method_function_map), count.index))}" : var.handler}"
-  timeout       = "${var.function_timeout}"
-  runtime       = "${var.runtime}"
+  count         = var.enabled ? length(keys(var.method_function_map)) : 0
+  s3_bucket     = var.s3_bucket_id
+  s3_key        = element(values(var.method_function_map), count.index)
+  function_name = var.cluster_name}_${var.group_name}_${lower(element(keys(var.method_function_map), count.index))
+  role          = element(aws_iam_role.lambda_role.*.arn, count.index)
+  handler       = var.runtime == "go1.x" ? "${var.group_name}_${lower(element(keys(var.method_function_map), count.index))}" : var.handler
+  timeout       = var.function_timeout
+  runtime       = var.runtime
 
   environment {
-    variables = "${var.method_env_map["${element(keys(var.method_function_map), count.index)}"]}"
+    variables = var.method_env_map[element(keys(var.method_function_map), count.index)]
   }
 }
 
 resource "aws_lambda_permission" "rest_function_api_gateway_permission" {
-  count         = "${var.enabled ? length(keys(var.method_function_map)) : 0}"
+  count         = var.enabled ? length(keys(var.method_function_map)) : 0
   statement_id  = "${var.cluster_name}_${lower(element(keys(var.method_function_map), count.index))}_allow_execution_from_gateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${element(aws_lambda_function.rest_function.*.function_name, count.index)}"
+  function_name = element(aws_lambda_function.rest_function.*.function_name, count.index)
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${var.rest_api_execution_arn}/*/*/*"
 }
 
 resource "aws_lambda_permission" "rest_function_release_bucket_permission" {
-  count         = "${var.enabled ? length(keys(var.method_function_map)): 0}"
+  count         = var.enabled ? length(keys(var.method_function_map)): 0
   statement_id  = "${var.cluster_name}_${lower(element(keys(var.method_function_map), count.index))}_release_bucket_permission"
   action        = "lambda:InvokeFunction"
-  function_name = "${element(aws_lambda_function.rest_function.*.arn, count.index)}"
+  function_name = element(aws_lambda_function.rest_function.*.arn, count.index)
   principal     = "s3.amazonaws.com"
-  source_arn    = "${var.s3_bucket_arn}"
+  source_arn    = var.s3_bucket_arn
 }
